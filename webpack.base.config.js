7a0f8be467822c0c01b89a9fc7e9301278fc4a4a
fjcalzado <fjcalzadosp@gmail.com>
2017-06-18 20:05:23 +0200

    - Fix bug in Webpack configuration with SVG files.

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -30,7 +30,7 @@ module.exports = {
     },
     // *** Images ***
     {
-      test: /\.(png|svg|jpg|gif|bmp)$/,
+      test: /\.(png|jpg|gif|bmp)$/,
       exclude: [/node_modules/],
       use: [{
         loader: "file-loader",
